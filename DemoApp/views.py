0ade0a114c9dfda5b1c3a52f66217d59de959acd
King Ask <kingask1234@gmail.com>
2022-07-23 17:10:03 +0545

    Changes the order of items display to random

--- DemoApp/views.py
+++ DemoApp/views.py
@@ -4,22 +4,22 @@ from django.http import HttpResponse
 from DemoApp.models import Products
 # Create your views here.
 def temp(req):
-    detail = Products.objects.all()
+    detail = Products.objects.all().order_by('?')
     # print("the details ----> ", detail.values())
     return render(req,'DemoApp/index.html',{'detail':detail})
 
 def wine(req):
-    detail = Products.objects.filter(productCategory = 'Wine')
+    detail = Products.objects.filter(productCategory = 'Wine').order_by('?')
     return render(req, 'DemoApp/products/wine.html',{'detail':detail})
 
 def beer(req):
-    detail = Products.objects.filter(productCategory = 'Beer')
+    detail = Products.objects.filter(productCategory = 'Beer').order_by('?')
     return render(req, 'DemoApp/products/beer.html',{'detail':detail})
 
 def champagne(req):
-    detail = Products.objects.filter(productCategory = 'Champagne')
+    detail = Products.objects.filter(productCategory = 'Champagne').order_by('?')
     return render(req, 'DemoApp/products/champagne.html',{'detail':detail})
 
 def vodka(req):
-    detail = Products.objects.filter(productCategory = 'Vodka')
+    detail = Products.objects.filter(productCategory = 'Vodka').order_by('?')
     return render(req, 'DemoApp/products/vodka.html',{'detail':detail})
