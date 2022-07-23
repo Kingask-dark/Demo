4a4d38755e52ea778f8e4cbefe92d964d7ddec86
King Ask <kingask1234@gmail.com>
2022-07-23 11:08:49 +0545

    Project Completed

--- DemoApp/views.py
+++ DemoApp/views.py
@@ -5,21 +5,21 @@ from DemoApp.models import Products
 # Create your views here.
 def temp(req):
     detail = Products.objects.all()
-    print("the details ----> ", detail.values())
+    # print("the details ----> ", detail.values())
     return render(req,'DemoApp/index.html',{'detail':detail})
 
 def wine(req):
-    detail = Products.objects.all()
+    detail = Products.objects.filter(productCategory = 'Wine')
     return render(req, 'DemoApp/products/wine.html',{'detail':detail})
 
 def beer(req):
-    detail = Products.objects.all()
+    detail = Products.objects.filter(productCategory = 'Beer')
     return render(req, 'DemoApp/products/beer.html',{'detail':detail})
 
 def champagne(req):
-    detail = Products.objects.all()
+    detail = Products.objects.filter(productCategory = 'Champagne')
     return render(req, 'DemoApp/products/champagne.html',{'detail':detail})
 
 def vodka(req):
-    detail = Products.objects.all()
+    detail = Products.objects.filter(productCategory = 'Vodka')
     return render(req, 'DemoApp/products/vodka.html',{'detail':detail})
