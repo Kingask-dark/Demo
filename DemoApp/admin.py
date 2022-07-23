10dce606527f13e10aceeff2eb0bcb5c3bd4f26c
King Ask <kingask1234@gmail.com>
2022-07-23 22:34:36 +0545

    Modified Admin Page

--- DemoApp/admin.py
+++ DemoApp/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from DemoApp.models import Products
 
-# Register your models here.
-admin.site.register(Products)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('productCategory', 'productName' , 'productCost')
+    list_editable = ('productCost', )
+    list_filter = ('productCategory','productCost')
+    search_fields = ('productCategory', 'productName')
+    fieldsets = (
+        (None, {
+            'fields': (
+                'productCategory',
+                'productName',
+                'productCost',
+                'productImage',
+                'productDescription'
+            )
+        }),
+    )
+
+
+admin.site.register(Products,ProductAdmin)
